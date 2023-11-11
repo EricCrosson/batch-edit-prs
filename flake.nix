ce01cfde27caccc01f64a556c4abb0830577055a
Eric Crosson <eric.s.crosson@utexas.edu>
2023-11-11 17:05:25 -0600

    build(nix): specify default dev shell

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    devShells = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            actionlint.enable = true;
+            alejandra.enable = true;
+            prettier.enable = true;
+          };
+        };
+      };
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_20
+        ];
+        shellHook = ''
+          export PATH="$PWD/node_modules/.bin:$PATH"
+          ${checks.pre-commit-check.shellHook}
+        '';
+      };
+    });
+  };
+}
